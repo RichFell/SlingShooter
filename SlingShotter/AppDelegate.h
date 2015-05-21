124fabc6dca20f13ce1d2412d76e91e876c326c2
Rich Fellure <rfellure24@gmail.com>
2015-05-21 12:27:19 -0700

    Initial Commit

---
+++ SlingShotter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlingShotter
+//
+//  Created by Rich Fellure on 5/21/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlingShotter/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SlingShotter
+//
+
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
